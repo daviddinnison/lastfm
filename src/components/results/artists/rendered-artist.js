91b279a6866d7c094ca9cf10de2963c73b70a12f
daviddinnison <david.g.dinnison@gmail.com>
2018-02-26 17:55:17 -0500

    added base stylesheet

--- src/components/results/artists/rendered-artist.js
+++ src/components/results/artists/rendered-artist.js
@@ -4,11 +4,15 @@ import React from "react";
 //react native
 import { StyleSheet, Text, View } from "react-native";
 
+//styles
+import createStyles from "../../baseStyles";
+const styles = createStyles();
+
 class RenderedArtist extends React.Component {
   render() {
     return (
       <View>
-        <Text>{this.props.name}</Text>
+        <Text style={styles.header}>{this.props.name}</Text>
         <Text>{this.props.match}% match</Text>
       </View>
     );
